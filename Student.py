f77f568096481fe4f25a33f6add2e32bd62ce391
Mikhail Alkin <atarax92@bk.ru>
2022-05-11 20:18:59 +0300

    Second commit

--- Student.py
+++ Student.py
@@ -5,10 +5,11 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         self.courses_attached = []
-
+        self.lect_grades = {}
 
     def rate_lec(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached \
+                and course in lecturer.courses_in_progress:
             if course in lecturer.lect_grades:
                 lecturer.lect_grades[course] += [grade]
             else:
@@ -16,10 +17,16 @@ class Student:
         else:
             return 'Ошибка'
 
-
     def __str__(self):
-        return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {calc(grade_list_st)} \nКурсы в процессе изучения: {self.courses_in_progress[0]}, {self.courses_in_progress[1]} \nЗавершенные курсы: Введение в программирование"
+        return f"Студент \nИмя: {self.name} \nФамилия: {self.surname} \n" \
+               f"Средняя оценка за домашние задания: {calc(grade_list_st)} \n" \
+               f"Курсы в процессе изучения: {self.courses_in_progress} \n" \
+               f"Завершенные курсы: Введение в программирование"
 
+    # def add_course(self):
+    #     list_course = input('Введите названия курсов')
+    #     self.courses_attached = self.courses_attached.append(list_course)
+    #     return list_course
 
 
 class Mentor:
@@ -28,9 +35,9 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if isinstance(student, Student) and course in self.courses_attached \
+                and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -40,28 +47,26 @@ class Mentor:
 
 
 class Reviewer(Mentor):
-    def rate_rev(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
     def __str__(self):
-        return f"Имя: {self.name} \nФамилия: {self.surname}"
-
+        return f"Преподаватель \nИмя: {self.name} \nФамилия: {self.surname}"
 
 
 class Lecturer(Student, Mentor):
-    lect_grades = {}
 
     def __str__(self):
-        return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {calc(grade_list_lec)}"
+        return f"Лектор \nИмя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {calc(grade_list_lec)}"
+
+    def __eq__(self, other):
+        return grade_st == grade_lec
+
+    def __lt__(self, other):
+        return grade_st < grade_lec
+
+    def __gt__(self, other):
+        return grade_st > grade_lec
 
 
-def calc (main_list: list):
+def calc(main_list: list):
     if not main_list:
         return 0
 
@@ -74,36 +79,12 @@ def calc (main_list: list):
     return result
 
 
-class Comparison:
-
-    def __init__(self, grade_st, grade_lec):
-        self.grade_st = calc(grade_list_st)
-        self.grade_lec = calc(grade_list_lec)
-
-    def __eq__(self, other):
-        if self.grade_st==self.grade_lec:
-            return True
-        else:
-            return False
-
-    def __lt__(self, other):
-        if self.grade_st < self.grade_lec:
-            return True
-        else:
-            return False
-
-    def __le__(self, other):
-        if self.grade_st <= self.grade_lec:
-            return True
-        else:
-            return False
-
-
 best_student = Student('Ruoy', 'Eman')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['SQL']
 best_student.courses_attached += ['Python']
 best_student.courses_attached += ['SQL']
+
 
 cool_mentor = Mentor('Men', 'Tor')
 cool_mentor.courses_attached += ['Python']
@@ -118,12 +99,12 @@ cool_lecturer.courses_in_progress += ['Python']
 cool_lecturer.courses_in_progress += ['SQL']
 
 
-reviewer.rate_rev(best_student, 'Python', 10)
-reviewer.rate_rev(best_student, 'Python', 9)
-reviewer.rate_rev(best_student, 'Python', 8)
+reviewer.rate_hw(best_student, 'Python', 10)
+reviewer.rate_hw(best_student, 'Python', 9)
+reviewer.rate_hw(best_student, 'Python', 8)
 
-reviewer.rate_rev(best_student, 'SQL', 10)
-reviewer.rate_rev(best_student, 'SQL', 10)
+reviewer.rate_hw(best_student, 'SQL', 10)
+reviewer.rate_hw(best_student, 'SQL', 10)
 
 best_student.rate_lec(cool_lecturer, 'Python', 10)
 best_student.rate_lec(cool_lecturer, 'Python', 8)
@@ -142,6 +123,8 @@ for key, value in cool_lecturer.lect_grades.items():
     for item in value:
         grade_list_lec.append(item)
 
+grade_st = calc(grade_list_st)
+grade_lec = calc(grade_list_lec)
 
 print(best_student)
 print()
